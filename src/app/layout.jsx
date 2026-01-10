6410ff4b9c000a615035b3ec542169ba57d26861
cherltte <cherltte@gmail.com>
2026-01-10 19:44:45 +0900

    feat: Add Google Tag Manager scripts to root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,10 +24,31 @@ export default async function RootLayout({ children }) {
     return (
         <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning className={`${montserrat.variable}`}>
             <head>
+                {/* Google Tag Manager */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TR4RCJLQ');`,
+                    }}
+                />
+                {/* End Google Tag Manager */}
                 {/* Preconnect for other assets if needed */}
                 <link rel="stylesheet" as="style" crossOrigin="anonymous" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" />
             </head>
             <body className="antialiased bg-[#F4F3EF]">
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-TR4RCJLQ"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 {children}
             </body>
         </html>
